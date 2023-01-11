26ab5a6cc25985453d04357168728ed27629e292
Miteshvaghela <vaghela_miteshk@yahoo.com>
2023-01-12 00:59:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,14 +7,35 @@ import Tasks from './components/Tasks';
 
 const App = ({}) => {
   const [showAddForm, setShowAddFrom] = useState(false);
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getAllTasks = async () =>{
+        const data = await fetchTasks();
+
+        setTasks(data);
+    }
+    getAllTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const allTasks = await fetch('http://localhost:8000/tasks');
+    const res = await allTasks.json();
+    return res;
+  }
+
+  const fetchTask = async (id) => {
+    const task = await fetch(`http://localhost:8000/tasks/${id}`);
+    const res = await task.json();
+    return res;
+  }
 
 
   const onAddForm = () => {
     setShowAddFrom(!showAddForm);    
   }
 
-  const onSaveTask = (obj) => {
+  const onSaveTask = async (obj) => {
     
     const newTask = {
       id : Math.floor(Math.random()*1000)+1,
@@ -22,14 +43,41 @@ const App = ({}) => {
       day : obj.day,
       reminder : obj.reminder
     }
+
+    await fetch('http://localhost:8000/tasks',{
+      method : 'post',
+      body : JSON.stringify(newTask),
+      headers : {
+        'content-type':'application/json'
+      }
+    })
+
     setTasks([...tasks, newTask]);
   } 
 
-  const toggleMe = (id) => {
+  const toggleMe = async (id) => {
+
+    const obj = await fetchTask(id);
+
+    obj.reminder = !obj.reminder;
+    console.log(obj);
+
+    await fetch(`http://localhost:8000/tasks/${id}`, {
+      method : 'put',
+      headers : {
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(obj)
+    });
+
     setTasks(tasks.map(task => (task.id == id)?{...task, reminder:!task.reminder}:task));
   }
 
-  const deleteMe = (id) => {
+  const deleteMe = async (id) => {
+
+    await fetch(`http://localhost:8000/tasks/${id}`, {
+      method : 'delete'
+    })
     setTasks(tasks.filter(task => (task.id !== id)));
   }
 
