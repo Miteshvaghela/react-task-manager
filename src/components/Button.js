03f4a6f1de3593b57c470d6c5c950a17ce2ddf5b
Miteshvaghela <vaghela_miteshk@yahoo.com>
2023-01-19 15:03:27 +0530

    upload

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'; 
-
-const Button = ({title, color, showAddForm}) => {
-    return (
-        <button style={{backgroundColor:color}} className="btn" onClick={showAddForm}>{title}</button>
-    )
-}
-
-export default Button;
