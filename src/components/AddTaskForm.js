39fdbcdf079924dbe9b9bf844b0806f508cf3475
Miteshvaghela <vaghela_miteshk@yahoo.com>
2023-06-20 11:27:15 +0530

    update

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -2,9 +2,7 @@ import { useState } from 'react'
 import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
-
 const AddTaskForm = ({ addTask }) => {
-
     const [title, setTitle] = useState('');
     const [day, setDay] = useState(new Date());
     const [reminder, setReminder] = useState(false);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react'; 
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; 
 import './App.css';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
+import ReportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-reportWebVitals(); 
+ReportWebVitals();
