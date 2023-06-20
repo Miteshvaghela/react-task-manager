cbd44d3b5331288f9251e62f15eb99b56931d9ff
Miteshvaghela <vaghela_miteshk@yahoo.com>
2023-06-20 11:29:00 +0530

    update

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -3,6 +3,7 @@ import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
 const AddTaskForm = ({ addTask }) => {
+
     const [title, setTitle] = useState('');
     const [day, setDay] = useState(new Date());
     const [reminder, setReminder] = useState(false);
